66f08c77dc0de124042eb940d27688d68e071d48
Himanshu Jagtap <Himanshu.HimJ@Gmail.com>
2025-04-10 13:39:43 +0530

    Set Up the Project

---
+++ config/config.py
@@ -0,0 +1,6 @@
+# AngelOne SmartAPI configuration
+# Historical API
+API_KEY = "UVGO6lSM"
+SECRET_KEY = "89c0217a-d602-4103-ae55-418937fbf455"
+CLIENT_ID = "your_client_id"
+TOTP_SECRET = "your_totp_secret"
